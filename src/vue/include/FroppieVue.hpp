548e03a4a292178b46c83c98e1a74e0247957d37
BarbierF <f-barbier@outlook.fr>
2016-10-30 19:17:17 +0100

    Ajout arborescence

---
+++ src/vue/include/FroppieVue.hpp
@@ -0,0 +1,36 @@
+#ifndef FROPPIE_VUE_HPP
+#define FROPPIE_VUE_HPP
+
+#include <gtkmm/main.h>
+#include <gtkmm/window.h>
+#include <gtkmm/action.h>
+#include <gtkmm/actiongroup.h>
+#include <gtkmm/box/h>
+#include <gtkmm/stock.h>
+#include <gtkmm/uimanager.h>
+#include <gtkmm/aboutdialog.h>
+#include <gtkmm/cellrendererpixbug.h>
+
+#
+
+namespace froppieLand{
+    namespace vue{
+
+        class FroppieVue: public Gtk::Window{
+        public:
+
+            friend class GFroppie;
+            friend class GSurface;
+
+
+        
+
+        private:
+
+            std:map
+
+        };
+    }
+}
+
+#endif
