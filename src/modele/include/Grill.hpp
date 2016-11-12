d6362aad9ac06e22c3276f3cc1c4f65b605335fb
IgnisMagister <chollet.florian7@gmail.com>
2016-11-12 18:04:38 +0100

    [Documentation] Grille
    Documentation de la grille

--- src/modele/include/Grill.hpp
+++ src/modele/include/Grill.hpp
@@ -21,7 +21,9 @@ namespace froppieLand{
             typedef nenuphar::Surface Surface;
             
         public:
-            
+            /**
+             * Constructeur d'une grille selon sa taille et la position de Départ et d'Arrivée
+             */
             Grill(
                 unsigned int taille
                 , unsigned int posLigneD
@@ -30,36 +32,90 @@ namespace froppieLand{
                 , unsigned int posColonneA
             );
 
+            /**
+             * Suppression du constructeur par recopie
+             */
             Grill(const Grill& ) = delete;
 
+            /**
+             * Suppression de l'opérateur d'attribution 
+             */
             Grill& operator=(const Grill&)const = delete;
 
         public:
-
+            /**
+             * Getter de la taille
+             * @return la taille de la grille
+             */
             const unsigned int& getTaille(void)const;
 
+            /**
+             * Getter de la position de départ
+             * @return Position de départ 
+             */
             const Position& getDepart(void)const;
-
+            
+            /**
+             * Getter de la position d'arrivée
+             * @return Position d'arrivée
+             */
             const Position& getArrivee(void)const;
-
+            
+            /**
+             * Getter de froppie
+             * @return Froppie (const)
+             */
             const Froppie& getFroppie(void)const;
-
+            
+            /**
+             * Getter de froppie en mode non constant
+             * @return Froppie
+             */
             Froppie& getModifFroppie(void);
-
+            
+            /**
+             * Getter de la surface à la position renseignée
+             * @param ligne Composante X de la position
+             * @param colonne Composante Y de la position
+             * @return la surface correspondante (const)
+             */
             const Surface& getSurface(const unsigned int& ligne, const unsigned int& colonne)const;
-
+            
+            /**
+             * Getter de la surface à la position renseignée en mode non constant
+             * @param ligne Composante X de la position
+             * @param colonne Composante Y de la position
+             * @return la surface correspondante
+             */
             Surface& getSurfaceModifiable(const unsigned int& ligne, const unsigned int& colonne);
             
+            /**
+             * Getter de la surface à la position de Froppie
+             * @param ligne Composante X de la position
+             * @param colonne Composante Y de la position
+             * @return la surface correspondante (const)
+             */
             const Surface& getFroppieSurf()const;
-
+            
+            /**
+             * Getter de la surface à la position de Froppie en mode non constant
+             * @param ligne Composante X de la position
+             * @param colonne Composante Y de la position
+             * @return la surface correspondante
+             */
             Surface& getModifFroppieSurf();
 
         public:
-
+            
             void construireChemin(void);
-
+            /**
+             * Action de vieillissement sur toutes les cases (donc nénuphars) de la grille
+             */
             void vieillissement();
-
+            /**
+             * Action de déplacement dans un vecteur donné
+             * @param le vecteur de translation
+             */
             void deplacerFroppie(const Direction& );
 
         protected:
@@ -67,13 +123,23 @@ namespace froppieLand{
             void adapterSurface(const unsigned int& ligne, const unsigned int& colonne);
 
         protected:
-
+            /**
+             * Champ de stockage de Froppie
+             */
             Froppie _froppie;
 
+            /**
+             * Taille de la grille
+             */
             unsigned int _taille;
-
+            /**
+             *  Positions de départ et d'arrivée
+             */
             const Position _depart, _arrivee;
 
+            /**
+             * Ensemble des surfaces décrivant le terrain de jeu
+             */
             std::vector< Surface > _terrain;
 
         };
