937c19d1c05aa9de155749d04bb42ee181a37b66
IgnisMagister <chollet.florian7@gmail.com>
2016-11-12 15:54:49 +0100

    [Documentation] Position
    Package froppieLand::modele

--- src/modele/include/Position.hpp
+++ src/modele/include/Position.hpp
@@ -5,26 +5,51 @@
 
 namespace froppieLand{
     namespace modele{
-
+        /**
+         * Classe décrivant la position d'un élément
+         */
         class Position{
 
             public:
-
+                /**
+                 * Constructeur par coordonnées
+                 */
                 Position(unsigned int& ligne, unsigned int& colonne);
+                /**
+                 * Constructeur par coordonnées constantes
+                 */
                 Position(const unsigned int& ligne, const unsigned int& colonne);
 
+                /**
+                 * Getter de la position dans l'axe X (les lignes)
+                 */
                 const unsigned int& getLigne()const;
+                /**
+                 * Getter de la position dans l'axe Y (les colonnes)
+                 */
                 const unsigned int& getColonne()const;
 
+                /**
+                 * Fonction de comparaison de positions
+                 */
                 bool operator<(const Position& )const;
 
             public:
-
+                /**
+                 * Translation de position selon un vecteur Direction
+                 */
                 void changeValue(const Direction& );
 
             protected:
 
+                /**
+                 * Composante X de la position
+                 */
                 unsigned int _ligne;
+
+                /**
+                 * Composante Y de la position
+                 */
                 unsigned int _colonne;
         };
     }
